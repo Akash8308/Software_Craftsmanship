3e5a06802d5f7e33d046dbc3cca14f8e1a10bebb
Akash <86683095+Akash8308@users.noreply.github.com>
2025-07-10 23:01:21 +0530

    added Domain service tests

--- Notes and Assignments/Problems/DDD/unusualSpends/src/test/java/com/sc/ddd/unusualSpends/domain/Service/UnusualSpendsProcessorTest.java
+++ Notes and Assignments/Problems/DDD/unusualSpends/src/test/java/com/sc/ddd/unusualSpends/domain/Service/UnusualSpendsProcessorTest.java
@@ -1,7 +1,81 @@
 package com.sc.ddd.unusualSpends.domain.Service;
 
+import com.sc.ddd.unusualSpends.DTO.SpendingByCategoryAndAmount;
+import com.sc.ddd.unusualSpends.DTO.UnusualSpend;
+import com.sc.ddd.unusualSpends.config.UnusualSpendsConfig;
+import com.sc.ddd.unusualSpends.domain.valueobject.SpendingCategory;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UnusualSpendsProcessorTest {
+    private final UnusualSpendsProcessor testSubject = new UnusualSpendsProcessor();
+
+    private static List<SpendingByCategoryAndAmount> getSpendingByCategory(SpendingCategory category, Double amount) {
+        return List.of(
+                new SpendingByCategoryAndAmount(category, amount)
+        );
+    }
+
+    @Test
+    void shouldBeAbleToGetUnusualSpendsWhenExceedThreshold() {
+        var lastMonthSpendingByCategoryAndAmountList = getSpendingByCategory(SpendingCategory.TRAVEL, 100.0);
+        var currentMonthSpendingByCategoryAndAmountList = getSpendingByCategory(SpendingCategory.TRAVEL, 3000.0);
+        var unusualSpendsConfig = new UnusualSpendsConfig(50.0);
+        var expectedUnusualSpend1 = new UnusualSpend(SpendingCategory.TRAVEL, 3000.0);
+        var expected = List.of(expectedUnusualSpend1);
+
+        // Act
+        var actual = testSubject.getUnusualSpending(
+                lastMonthSpendingByCategoryAndAmountList,
+                currentMonthSpendingByCategoryAndAmountList,
+                unusualSpendsConfig
+        );
+
+        // Assert
+        assertTrue(expected.containsAll(actual));
+    }
+
+    @Test
+    void shouldBeAbleToReturnEmptyUnusualSpendsIfNotExceededThreshold() {
+        // Arrange
+        var lastMonthSpendingByCategoryAndAmountList = getSpendingByCategory(SpendingCategory.FOOD, 200.0);
+        var currentMonthSpendingByCategoryAndAmountList = getSpendingByCategory(SpendingCategory.FOOD, 250.0);
+        var unusualSpendsConfig = new UnusualSpendsConfig(50.0);
+
+        // Act
+        var actual = testSubject.getUnusualSpending(
+                lastMonthSpendingByCategoryAndAmountList,
+                currentMonthSpendingByCategoryAndAmountList,
+                unusualSpendsConfig
+        );
+
+        // Assert
+        assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    void shouldBeAbleToReturnUnusualSpendsIfMetThreshold() {
+        // Arrange
+        var lastMonthSpendingByCategoryAndAmountList = getSpendingByCategory(SpendingCategory.FOOD, 100.0);
+        var currentMonthSpendingByCategoryAndAmountList = getSpendingByCategory(SpendingCategory.FOOD, 150.0);
+        var unusualSpendsConfig = new UnusualSpendsConfig(50.0);
+        var expected = List.of(
+                new UnusualSpend(SpendingCategory.FOOD, 150.0)
+        );
+
+        // Act
+        var actual = testSubject.getUnusualSpending(
+                lastMonthSpendingByCategoryAndAmountList,
+                currentMonthSpendingByCategoryAndAmountList,
+                unusualSpendsConfig
+        );
+
+        // Assert
+        assertTrue(actual.containsAll(expected));
+    }
+
 
 }
--- Notes and Assignments/Problems/DDD/unusualSpends/src/test/java/com/sc/ddd/unusualSpends/domain/entity/CreditCardTest.java
+++ Notes and Assignments/Problems/DDD/unusualSpends/src/test/java/com/sc/ddd/unusualSpends/domain/entity/CreditCardTest.java
@@ -26,5 +26,5 @@ class CreditCardTest {
         var transactions = creaditCard.getTransactionIds();
         assertTrue(transactions.isEmpty());
     }
-    
+
 }
