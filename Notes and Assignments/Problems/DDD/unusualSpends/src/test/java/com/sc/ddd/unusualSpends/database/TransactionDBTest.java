a08ddbd7ff4dbd0e7c71daca9ccf616c4b708917
Akash <86683095+Akash8308@users.noreply.github.com>
2025-07-13 18:55:38 +0530

    Added transactionDb Tests

---
+++ Notes and Assignments/Problems/DDD/unusualSpends/src/test/java/com/sc/ddd/unusualSpends/database/TransactionDBTest.java
@@ -0,0 +1,40 @@
+package com.sc.ddd.unusualSpends.database;
+
+import com.sc.ddd.unusualSpends.domain.entity.CreditCard;
+import com.sc.ddd.unusualSpends.domain.entity.Transaction;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionDBTest {
+
+    @Test
+    void shouldCreateEmptyTransactionDB(){
+        TransactionDB transDb = new TransactionDB();
+
+        var transactions = transDb.getTransactions();
+
+        assertTrue(transactions.isEmpty());
+    }
+
+    @Test
+    void shouldReturnAllTransactionDB(){
+        TransactionDB transDb = new TransactionDB();
+
+        Transaction t1 = new Transaction("T01", 100, "M01", "C01", LocalDateTime.now());
+        Transaction t2 = new Transaction("T02", 200, "M01", "C01", LocalDateTime.now());
+        Transaction t3 = new Transaction("T03", 300, "M01", "C01", LocalDateTime.now());
+
+        transDb.addTransaction(t1);
+        transDb.addTransaction(t2);
+        transDb.addTransaction(t3);
+
+        var transactions = transDb.getTransactions();
+
+        assertTrue(transactions.isEmpty());
+    }
+
+
+}
